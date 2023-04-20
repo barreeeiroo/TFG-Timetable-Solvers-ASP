c9cd54b42d8ef7728613b92bb64383455446f662
Diego Barreiro <diego.barreiro.perez@gmail.com>
2023-04-20 02:15:37 +0200

    Provide Example Output

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-import logging as logger
 from pathlib import Path
 
 from adapter.asp.scheduler import AspSolver
@@ -10,34 +9,28 @@ from sdk.aws_s3 import get_input_object, save_output_object
 
 def aws_execution(execution_arn: str):
     execution_uuid = execution_arn.split(":")[-1]
-    logger.info(f"Execution UUID: {execution_uuid}")
+    print(f"Execution UUID: {execution_uuid}")
 
     input_data = get_input_object(execution_uuid)
-    logger.info("Received INPUT")
-    logger.info(input_data)
 
-    logger.info("Creating ASP Solver")
     solver = AspSolver(input_data.sessions, input_data.rooms, input_data.settings)
     solver.with_execution_uuid(execution_uuid)
-    logger.info("Invoking ASP Solver")
     output = solver.solve()
 
-    logger.info("Storing OUTPUT")
     object_key = save_output_object(execution_uuid, output)
-    return {
-        "result": object_key
-    }
 
 
 def local_execution(input_file_path_raw: str):
     input_file_path = Path(input_file_path_raw)
-    with open(Path(input_file_path)) as f:
+    with open(input_file_path) as f:
         data = json.loads(f.read())
     input_data = SolverInput.parse_obj(data)
 
     solver = AspSolver(input_data.sessions, input_data.rooms, input_data.settings)
     output = solver.solve()
-    print(output.json())
+
+    with open(input_file_path.parent / 'output.json', 'w') as f:
+        f.write(output.json(exclude_none=True) + "\n")
 
 
 if __name__ == "__main__":
