bc64b7c307c2a545da4d6fcffb7d9f247f5dedb9
Diego Barreiro <diego.barreiro.perez@gmail.com>
2023-05-07 13:09:11 +0200

    Standarize Preference Type

--- src/adapter/asp/optimizations.py
+++ src/adapter/asp/optimizations.py
@@ -2,16 +2,27 @@ from enum import Enum
 
 
 class OptimizationPriorities(int, Enum):
-    UNDESIRABLE_TIMESLOT_5 = 3
-    UNDESIRABLE_TIMESLOT_2 = 2
-    UNDESIRABLE_TIMESLOT_1 = 1
+    UNDESIRABLE_TIMESLOT_5 = 4
+    UNDESIRABLE_TIMESLOT_2 = 3
+    UNDESIRABLE_TIMESLOT_1 = 2
+    ROOM_PREFERENCE_FOR_SESSION = 1
 
 
 class PenaltyNames(str, Enum):
     UNDESIRABLE_TIMESLOT = '"UndesirableTimeslot"'
+    AVOID_ROOM_FOR_SESSION = '"AvoidRoomForDegree"'
 
 
 class PenaltyCosts(int, Enum):
     UNDESIRABLE_TIMESLOT_1 = 10
+    AVOID_ROOM_FOR_SESSION = 15
     UNDESIRABLE_TIMESLOT_2 = 20
     UNDESIRABLE_TIMESLOT_5 = 50
+
+
+class BonusNames(str, Enum):
+    PREFER_ROOM_FOR_SESSION = '"PreferRoomForSession"'
+
+
+class BonusCosts(int, Enum):
+    PREFER_ROOM_FOR_SESSION = 15
