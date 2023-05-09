d95bb456093f388f2e9dbeb5bec65f7e390fa36e
Diego Barreiro <diego.barreiro.perez@gmail.com>
2023-05-09 20:06:13 +0200

    Reduce ASP Problem Size

--- src/utils/slot_utils.py
+++ src/utils/slot_utils.py
@@ -17,3 +17,11 @@ def generate_sub_slots(full_slot: Slot, slot_duration: timedelta) -> List[Slot]:
         ))
         timeframe = Timeframe(start=timeframe.end, end=add_time(timeframe.end, slot_duration))
     return slots
+
+
+def generate_slot_groups(nums: List[int]) -> str:
+    nums = sorted(set(nums))
+    gaps = [[s, e] for s, e in zip(nums, nums[1:]) if s + 1 < e]
+    edges = iter(nums[:1] + sum(gaps, []) + nums[-1:])
+    ranges = list(zip(edges, edges))
+    return ";".join([str(a) if a == b else f"{a}..{b}" for a, b in ranges])
